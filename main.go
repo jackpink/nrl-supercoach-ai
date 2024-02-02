daf7492e74458cfad787f9bf6f22f09d6ad5aa3e
Jack-Desktop <jackpinkerton@live.com.au>
2024-02-02 17:59:24 +1100

    first commit

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"fmt"
+	"encoding/json"
+)
+
+// templateData provides template parameters.
+type templateData struct {
+	Service  string
+	Revision string
+}
+
+// Variables used to generate the HTML page.
+var (
+	data templateData
+	tmpl *template.Template
+)
+
+func main() {
+	// Initialize template parameters.
+	service := os.Getenv("K_SERVICE")
+	if service == "" {
+		service = "???"
+	}
+
+	revision := os.Getenv("K_REVISION")
+	if revision == "" {
+		revision = "???"
+	}
+
+	// Prepare template for execution.
+	tmpl = template.Must(template.ParseFiles("index.html"))
+	data = templateData{
+		Service:  service,
+		Revision: revision,
+	}
+
+	// Define HTTP server.
+	http.HandleFunc("/", helloRunHandler)
+
+	fs := http.FileServer(http.Dir("./assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	// PORT environment variable is provided by Cloud Run.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Print("Hello from Cloud Run! The container started successfully and is listening for HTTP requests on $PORT")
+	log.Printf("Listening on port %s", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// helloRunHandler responds to requests by rendering an HTML page.
+func helloRunHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fruits := make(map[string]int)
+    fruits["Apples"] = 25
+    fruits["Oranges"] = 10
+	enableCors(&w)
+	return json.NewEncoder(w).Encode(fruits)
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
