61aa9f925440042c6b10db0f3e43b4315e7227f4
Jack-Desktop <jackpinkerton@live.com.au>
2024-02-09 16:48:45 +1100

    for loops updated

--- internal/selection/selection.go
+++ internal/selection/selection.go
@@ -23,13 +23,52 @@ type squad struct {
 	}
 }
 
+type fullbacks struct {
+	players [2]player
+}
+
+func (f fullbacks) isNotFull() boolean {
+	for player in f.players {
+		if player.id == 0{
+			return true
+		}
+	}
+	return false
+}
+
+func (f* fullbacks) addPlayer(p player) boolean {
+	if f.isNotFull() {
+		// add player
+		for player in f.players {
+			if player.id == 0 {
+				player.id = .id
+				player.name = p.name
+				player.position1 = p.position1
+				player.position2 = p.position2
+				player.price = p.price
+				player.team = p.team
+			}
+		}
+		return true
+	}
+	return false
+}
+
 // Map for positions needed (ie FRF to FrontRowForward)
 
 func (squad* squad) addPlayer(player player) {
 	// try to add first position
-
+	position := positions(player.position1)
+	added, err := squad.position.addPlayer(player)
 	// if unsuccessfull try to add second position
+	if !added {
+		addedBySecondPosition, err := squad.position.addPlayer(player)
+	}
 	// if unsuccessful try to add via player swap
+	if !ad {
+		addedByPlayerSwap
+	}
+	if 
 }
 
 func selection(year int, numberOfRookies int) {
@@ -44,7 +83,7 @@ func selectRookies(squad Squad, year int, numberOfRookies int) Squad {
 	playersAdded := 0
 	squad, playersAdded = selectStartingRookies()
 	squad, playersAdded = selectBenchRookies()
-	while playersAdded < numberOfRookies {
+	for playersAdded < numberOfRookies {
 		// increment price by $50,000
 		squad, playersAdded = selectCheapiesInRange()
 	}
@@ -52,8 +91,14 @@ func selectRookies(squad Squad, year int, numberOfRookies int) Squad {
 
 func selectStartingRookies(playersAdded int, numberOfRoookies int, squad Squad) Squad {
 	var startingRookies []Player
-	while playersAdded < numberOfRookies {
-		squad.addPlayer(startingRookies.pop())
+	for i := 0; i < len(startingRookies); i++ {
+		playerAdded := squad.addPlayer(startingRookies[i])
+		if playerAdded {
+			playersAdded++
+		}
+		if playersAdded == numberOfRoookies {
+			return squad
+		}
 	}
 	return squad
 
